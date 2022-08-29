f3995ea165f5b999eabdd4cb55ce842466fb265a
randoelicor <Jose_Leonard_Rico@manulife.ca>
2022-08-30 00:45:34 +0800

    Implemented HOC

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import React, { Component } from 'react';
 // import ThemeSelector from './components/ThemeSelector';
 import GeneralList from './components/GeneralList';
 import SortedList from './components/SortedList';
+import ProFeature from './components/ProFeature';
+
+
+const ProList = ProFeature(SortedList);
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       // counter: 0
-      names: ["Zoe", "Bob", "Alice", "Dora", "Joe"]
+      names: ["Zoe", "Bob", "Alice", "Dora", "Joe"],
+      cities: ["London", "New York", "Paris", "Milan", "Boston"],
+      proMode: false
     };
   }
 
@@ -18,15 +24,39 @@ export default class App extends Component {
   //   this.setState({ counter: this.state.counter + 1 });
   // }
 
+  toggleProMode = () => {
+    this.setState({ proMode: !this.state.proMode });
+  }
+
   render() {
     return (
      <div className="container-fluid">
         <div className="row">
-          <div className="col-6">
+          <div className="col-12 text-center p-2">
+            <div className="form-check">
+              <input type="checkbox" className="form-check-input" value={ this.state.proMode } onChange={ this.toggleProMode } />
+              <label className="form-check-label">Pro Mode</label>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          {/* <div className="col-6">
             <GeneralList list={ this.state.names } theme="primary" />
           </div>
           <div className="col-6">
             <SortedList list={ this.state.names } />
+          </div> */}
+          <div className="col-3">
+            <GeneralList list={ this.state.names } theme="primary" />
+          </div>
+          <div className="col-3">
+            <ProList list={ this.state.names } pro={ this.state.proMode } />
+          </div>
+          <div className="col-3">
+            <GeneralList list={ this.state.cities } theme="secondary" />
+          </div>
+          <div className="col-3">
+            <ProList list={ this.state.cities } pro={ this.state.proMode } />
           </div>
         </div>
      </div>
