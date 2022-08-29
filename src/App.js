cf7002385556126d3ddb558b1b91b3e28c0c429c
randoelicor <Jose_Leonard_Rico@manulife.ca>
2022-08-30 00:21:38 +0800

    Specialized component demo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
-import Message from './components/Message';
-import ActionButton from './components/ActionButton';
-import ThemeSelector from './components/ThemeSelector';
+// import Message from './components/Message';
+// import ActionButton from './components/ActionButton';
+// import ThemeSelector from './components/ThemeSelector';
+import GeneralList from './components/GeneralList';
+import SortedList from './components/SortedList';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0
+      // counter: 0
+      names: ["Zoe", "Bob", "Alice", "Dora", "Joe"]
     };
   }
 
-  incrementCounter = () => {
-    this.setState({ counter: this.state.counter + 1 });
-  }
+  // incrementCounter = () => {
+  //   this.setState({ counter: this.state.counter + 1 });
+  // }
 
   render() {
     return (
-      <ThemeSelector>
-        <div className="m-2 text-center">
-          <Message theme="primary" message={ `Counter: ${this.state.counter}` } />
-          <ActionButton theme="secondary" text="Increment" callback={ this.incrementCounter } />
+     <div className="container-fluid">
+        <div className="row">
+          <div className="col-6">
+            <GeneralList list={ this.state.names } theme="primary" />
+          </div>
+          <div className="col-6">
+            <SortedList list={ this.state.names } />
+          </div>
         </div>
-      </ThemeSelector>
+     </div>
     );
   }
 }
